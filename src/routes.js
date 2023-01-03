22d3f47fa581018a511eb2817fdaa5d3cfef3b32
Luiz Carlos Goulart <luizcarlosgoulart@outlook.com.br>
2023-01-03 01:28:47 -0300

    feat: 🎸 core config + routes

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const ClientController = require('./Controllers/ClientController')
+const routes = Router()
+
+routes.get('/health', (req, res) => {
+    return res.status(200).json({message: 'Server is on.'})
+})
+
+routes.post('/clients', ClientController.register)
+routes.get('/clients', ClientController.index)
+routes.get('/clients/:id', ClientController.show)
+routes.put('/clients/:id', ClientController.update)
+routes.delete('/clients/:id', ClientController.delete)
+
+module.exports = routes
